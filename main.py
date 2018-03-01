34f3f94c0805f0c72fcfe19edf7e0ccf4dff59f9
Eric Mertz <ericm338@Erics-MacBook-Pro-2.local>
2018-03-01 01:18:10 -0600

    creating recipe class

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Getting Frequency Dictionary"""
+"""run this"""
 
-from html.parser import HTMLParser
-from urllib import request
 import sys
-from bs4 import BeautifulSoup
-# create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
-    """a class overriding HTML parser"""
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        print("Encountered some data  :", data)
-
-def output(ingredients, directions):
-    print("INGREDIENTS\n")
-    for i in ingredients:
-        print(i + "\n")
-    print("DIRECTIONS\n")
-    for d in directions:
-        print(d + "\n")
+from recipe import Recipe
 
 
 def main():
@@ -33,35 +12,8 @@ def main():
     if len(sys.argv) > 1:
         url = sys.argv[1]
 
-    parser = MyHTMLParser()
-    
-    response = request.urlopen(url)
-    page_source = response.read()
-    soup = BeautifulSoup(page_source, 'html.parser')
-    # mydivs = soup.findAll('div')
-
-
-    ingredients = []
-    directions = []
-
-    mySpansIng = soup.findAll("span", {"class": "recipe-ingred_txt added"})
-    mySpansDir = soup.findAll("span", {"class": "recipe-directions__list--item"})
-
-
-    for li in mySpansIng:
-        ingredients.append(str(li.string))
-
-    for span in mySpansDir:
-        if span.string != None:
-            directions.append(str(span.string))
-
-    output(ingredients,directions)
-
-    # print(soup.prettify)
-
-    # print(soup.findall('recipe-directions__list--item'))
-    # print(page_source)
-    # parser.feed(page_source)
+    recipe = Recipe(url)
+    print(recipe)
 
 if __name__ == "__main__":
     main()
