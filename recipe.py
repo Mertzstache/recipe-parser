6a6d70da0d490319940a2eaa58fc6adb4d2378da
Will Hancock <whancock@gatech.edu>
2018-03-13 21:01:16 -0500

    making skeleton functions to be filled in

--- recipe.py
+++ recipe.py
@@ -33,21 +33,15 @@ class Recipe():
         return return_me
 
 
-    def substitute_ingredient(self, previous, new):
-        """this is a simple idea of replacing one WHOLE INGREDIENT DIRECTION with a new one"""
-        # self.ingredients.remove(previous)
-        # self.ingredients.append(new)
-        #look through all instances in directions and replace as well
 
+    def make_healthy(self):
         pass
 
+    def make_vegetarian(self):
+        pass
 
-
-    def transform_ingred(self, ttype):
-
-        for ingred in self.ingredients:
-            print(ingred)
-
+    def make_in_style(self):
+        pass
 
 
     def multiply_portion(self, multiplier):
@@ -61,3 +55,26 @@ class Recipe():
                 else:
                     output_arr.append(str(int(n)*multiplier))
             self.ingredients[i][0] = ' '.join(output_arr)
+
+
+
+
+
+    def _substitute_ingredient(self, previous, new):
+        """this is a simple idea of replacing one WHOLE INGREDIENT DIRECTION with a new one"""
+        # self.ingredients.remove(previous)
+        # self.ingredients.append(new)
+        #look through all instances in directions and replace as well
+
+        pass
+
+
+
+    def _transform_ingred(self, ttype):
+
+        for ingred in self.ingredients:
+            print(ingred)
+
+
+
+    
